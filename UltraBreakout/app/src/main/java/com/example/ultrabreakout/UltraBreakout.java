b29c9dd35a6d099d4170a932dbd93e35a3534c62
James Huang <James.Huang.R@gmail.com>
2019-07-09 18:01:48 -0700

    basic project setup

---
+++ UltraBreakout/app/src/main/java/com/example/ultrabreakout/UltraBreakout.java
@@ -0,0 +1,14 @@
+package com.example.ultrabreakout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UltraBreakout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
