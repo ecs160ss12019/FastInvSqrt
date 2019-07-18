882cea8c5b188ecdda7f0913ef3073f3ca2eab2e
Nicola Mussi <npmussi@gmail.com>
2019-07-17 18:54:25 -0700

    Added constructors for Brick and Paddle

--- UltraBreakout/app/src/main/java/com/example/ultrabreakout/Paddle.java
+++ UltraBreakout/app/src/main/java/com/example/ultrabreakout/Paddle.java
@@ -5,5 +5,14 @@ package com.example.ultrabreakout;
  * Basically just a rectangle that moves back and forth.
  */
 
+import android.graphics.Color;
+
 class Paddle extends Actor {
+    private static final int PADDLE_WIDTH = 160;
+    private static final int PADDLE_HEIGHT = 40;
+
+    public Paddle(float x_pos, float y_pos) {
+        super(x_pos, y_pos, 0, 0, PADDLE_WIDTH, PADDLE_HEIGHT,
+                Color.RED);
+    }
 }
