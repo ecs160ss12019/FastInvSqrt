65a57cc946cb28490286e0686a44010daf18a89a
Mohamed Sayed <mosayed@ucdavis.edu>
2019-07-17 18:55:23 -0700

    created paddle

--- UltraBreakout/app/src/main/java/com/example/ultrabreakout/Paddle.java
+++ UltraBreakout/app/src/main/java/com/example/ultrabreakout/Paddle.java
@@ -5,5 +5,14 @@ package com.example.ultrabreakout;
  * Basically just a rectangle that moves back and forth.
  */
 
+import android.graphics.Color;
+
 class Paddle extends Actor {
+
+
+    public Paddle(float x_pos, float y_pos, float x_vel, float y_vel) {
+        //FIXME: Come up with a standardized ball size
+        super(x_pos, y_pos, x_vel,y_vel, 40,40,
+                Color.GREEN);
+    }
 }
