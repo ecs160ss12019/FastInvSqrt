6951feff88117885bb1654672337f4b7f330171d
unknown <James.Huang.R@gmail.com>
2019-07-18 17:37:17 -0700

    combined Title class with UltraBreakout_TitleScreenActivity class

--- UltraBreakout/app/src/main/java/com/example/ultrabreakout/Title.java
+++ UltraBreakout/app/src/main/java/com/example/ultrabreakout/Title.java
@@ -1,16 +1,41 @@
 package com.example.ultrabreakout;
 
-import android.view.Display;
-import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * Handles the title screen and its navigation.
- *
- * TODO
- * The Title class at minimum has the title in text and a mode
- * select option; a background would be nice.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-class Title {
+public class Title extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        configureFullScreen();
+        configureLevelsButton();
+    }
+
+    //makes activity FullScreen and sets contentView
+    private void configureFullScreen(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_ultra_breakout__title_screen);
+    }
+
+    //Sets up On Click for Levels Button
+    private void configureLevelsButton() {
+        Button Levels_button = (Button) findViewById(R.id.LevelsButton);
+        Levels_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(Title.this, LevelSelectPopUpScreen.class));
+            }
+        });
+
+    }
 
 }
