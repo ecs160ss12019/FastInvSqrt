8d56751be5f703dde253b939a627cf7e34302599
Kamal Sadek <kjsadek@ucdavis.edu>
2019-07-25 14:01:31 -0700

    Document and clean the Sound controller class
    Gives a lot more instruction on how exactly the Sound Controller
    interacts with both the music and sound effects for the game.

--- UltraBreakout/app/src/main/java/com/example/ultrabreakout/Sound.java
+++ UltraBreakout/app/src/main/java/com/example/ultrabreakout/Sound.java
@@ -2,11 +2,9 @@ package com.example.ultrabreakout;
 
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.media.AudioAttributes;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
-import android.net.Uri;
 import android.util.Log;
 
 import java.io.IOException;
@@ -17,20 +15,32 @@ import java.io.IOException;
  * within the app.
  *
  * Sound is split into two categories, background music and sound effects.
- * Background music is simply set on a screen and set to loop when the activity is started.
- * Sound effects are manually called by the application when certain events happen.
+ * Background music is simply set on a screen and set to loop when the activity is started,
+ * and this is handled by the MediaPlayer, which streams in the music.
+ * Sound effects are manually called by the application when certain events happen, and these
+ * are handled by the SoundPool, since they are smaller in size than the music and can
+ * comfortably fit in the RAM.
  */
 public class Sound {
+    // Singleton instance.
     private static Sound instance;
 
+    // MediaPlayer handles streaming in the music.
     private MediaPlayer mediaPlayer;
+
+    // SoundPool handles all of the sound effects of the game.
     private SoundPool soundPool;
 
+    // Currently loaded resource id for the music, so we know what music is
+    // playing at any moment.
     private int resourceId;
 
+    // Whether the MediaPlayer has finished buffering the music.
     private boolean prepared;
 
     private Sound() {
+        // Set attributes for the sound pool, which signify that it will be used for
+        // short audio files, attached to user events.
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
@@ -42,13 +52,15 @@ public class Sound {
 
         resourceId = 0;
         prepared = false;
-
-        /*mediaPlayer = MediaPlayer.create(context, R.raw.background_1);
-        mediaPlayer.setLooping(true);
-        mediaPlayer.start();*/
     }
 
+    /* Play a certain audio file for the background music on repeat.
+     *
+     *
+     */
     public void play_background(Context context, int background_music_id) {
+        // Check to see if we're already playing the specified audio file.
+        // If so, just continue playing it.
         resourceId = background_music_id;
         if (mediaPlayer != null && resourceId == background_music_id) {
             if (prepared) {
@@ -61,6 +73,7 @@ public class Sound {
             mediaPlayer.release();
         }
 
+        // Get the file descriptor for the specified audio file, and pass it in to the MediaPlayer.
         AssetFileDescriptor afd = context.getResources().openRawResourceFd(resourceId);
         prepared = false;
         mediaPlayer = new MediaPlayer();
@@ -70,6 +83,8 @@ public class Sound {
             Log.e("Error", e.toString());
         }
         mediaPlayer.setLooping(true);
+
+        // Set callback, so it starts playing the audio file when it finishes loading it in.
         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
@@ -78,10 +93,6 @@ public class Sound {
             }
         });
         mediaPlayer.prepareAsync();
-    }
-
-    private void play_media() {
-
     }
 
     public static Sound getInstance() {
